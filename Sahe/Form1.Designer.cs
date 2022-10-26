b7fc2e67fa9f7c26601686006031818c287f01c9
MaxWell61121 <vanatizadehasan@gmail.com>
2022-10-26 13:12:50 +0400

    Task Completed

---
+++ Sahe/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Sahe
+{
+    partial class frm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtresult = new System.Windows.Forms.TextBox();
+            this.btnresult = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(390, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter the Dimensions of the figure";
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(239, 88);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(100, 22);
+            this.txtX.TabIndex = 1;
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(239, 124);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(100, 22);
+            this.txtY.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(47, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 27);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter Breadth :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Montserrat Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(47, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 27);
+            this.label3.TabIndex = 4;
+            this.label3.Text = " Enter Length  :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Montserrat Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(144, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 27);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Area :";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // txtresult
+            // 
+            this.txtresult.Location = new System.Drawing.Point(239, 158);
+            this.txtresult.Name = "txtresult";
+            this.txtresult.Size = new System.Drawing.Size(100, 22);
+            this.txtresult.TabIndex = 8;
+            // 
+            // btnresult
+            // 
+            this.btnresult.Location = new System.Drawing.Point(239, 205);
+            this.btnresult.Name = "btnresult";
+            this.btnresult.Size = new System.Drawing.Size(100, 45);
+            this.btnresult.TabIndex = 9;
+            this.btnresult.Text = "Find Area";
+            this.btnresult.UseVisualStyleBackColor = true;
+            this.btnresult.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnclear
+            // 
+            this.btnclear.Location = new System.Drawing.Point(133, 205);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(100, 45);
+            this.btnclear.TabIndex = 10;
+            this.btnclear.Text = "Clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            this.btnclear.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnexit
+            // 
+            this.btnexit.BackColor = System.Drawing.Color.IndianRed;
+            this.btnexit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnexit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnexit.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnexit.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnexit.Location = new System.Drawing.Point(361, -1);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(48, 40);
+            this.btnexit.TabIndex = 11;
+            this.btnexit.Text = "X";
+            this.btnexit.UseVisualStyleBackColor = false;
+            this.btnexit.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // frm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(407, 284);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnresult);
+            this.Controls.Add(this.txtresult);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frm1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sahe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtresult;
+        private System.Windows.Forms.Button btnresult;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Button btnexit;
+    }
+}
+
---
+++ Sahe/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sahe
+{
+    public partial class frm1 : Form
+    {
+        public frm1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x, y, z , result;
+            x = Convert.ToInt32(txtX.Text);
+            y = Convert.ToInt32(txtY.Text);
+            result = x * y ;
+            txtresult.Text = result.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtX.Clear();
+            txtY.Clear();
+          
+            txtresult.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); 
+        }
+    }
+}
